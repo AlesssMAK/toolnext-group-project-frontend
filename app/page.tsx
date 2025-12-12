e2a44c3c31bfd8bb00ce465e8b1a800eef6b4070
Sensei8760 <gnid.yura@gmail.com>
2025-12-12 09:25:05 +0200

    add-Task14_YuriiBenefitsBlock

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image';
 import styles from './page.module.css';
 import HeroBlock from "@/components/HeroBlock/HeroBlock";
+import BenefitsBlock from "@/components/BenefitsBlock/BenefitsBlock";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main>
         <HeroBlock />
+        <BenefitsBlock />
       </main>
     </div>
   );
