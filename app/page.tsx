89dc4ccfd1e72035bc675fe998b5f71e428c7c56
Oleksii <makovii88@gmail.com>
2025-12-12 18:03:11 +0100

    add sprite

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image';
 import styles from './page.module.css';
-import HeroBlock from "@/components/HeroBlock/HeroBlock";
-import BenefitsBlock from "@/components/BenefitsBlock/BenefitsBlock";
+import HeroBlock from '@/components/HeroBlock/HeroBlock';
+import BenefitsBlock from '@/components/BenefitsBlock/BenefitsBlock';
 import { FeaturedToolsBlock } from '@/components/FeaturedToolsBlock/FeaturedToolsBlock';
+import { FeedbacksBlock } from '@/components/FeedbacksBlock/FeedbacksBlock';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <HeroBlock />
         <BenefitsBlock />
         <FeaturedToolsBlock />
+        <FeedbacksBlock />
       </main>
     </div>
   );
