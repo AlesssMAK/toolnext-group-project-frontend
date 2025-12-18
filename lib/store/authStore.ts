6f271f6a4280601853b3d0ab72022df6bc937431
Oleksii <makovii88@gmail.com>
2025-12-18 17:04:11 +0100

    uodate user header

--- lib/store/authStore.ts
+++ lib/store/authStore.ts
@@ -3,18 +3,22 @@ import { User } from '@/types/user';
 
 type AuthStore = {
   isAuthenticated: boolean;
+  loading: boolean;
   user: User | null;
   setUser: (user: User) => void;
   clearIsAuthenticated: () => void;
+  setLoading: (value: boolean) => void;
 };
 
-export const useAuthStore = create<AuthStore>()((set) => ({
+export const useAuthStore = create<AuthStore>()(set => ({
   isAuthenticated: false,
   user: null,
+  loading: true,
+  setLoading: value => set(() => ({ loading: value })),
   setUser: (user: User) => {
-    set(() => ({ user, isAuthenticated: true }));
+    set(() => ({ user, isAuthenticated: true, loading: false }));
   },
   clearIsAuthenticated: () => {
-    set(() => ({ user: null, isAuthenticated: false }));
+    set(() => ({ user: null, isAuthenticated: false, loading: false }));
   },
 }));
