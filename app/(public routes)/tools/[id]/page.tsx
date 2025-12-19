0b26ea1253a7034f8fa235121ccda46e4e1b340d
Iryna-M-html <ivmohnataya@gmail.com>
2025-12-20 00:57:56 +0200

    beginner

--- app/(public routes)/tools/[id]/page.tsx
+++ app/(public routes)/tools/[id]/page.tsx
@@ -1,9 +1,21 @@
-import BookingToolPage from '@/components/BookingToolForm/BookingToolForm'
-const ToolDetails = () => {
+import BookingToolForm from '@/components/BookingToolForm/BookingToolForm';
+import Link from 'next/link';
+import styles from './BookingToolPage.module.css';
+
+type Props = {
+  toolId: string;
+  pricePerDay: number;
+};
+
+const BookingToolPage = ({ toolId, pricePerDay }: Props) => {
   return (
-    // <div>ToolDetails</div>
-    <BookingToolPage />
+    <section className={`${styles.containerConfirmation} container`}>
+      <BookingToolForm toolId={toolId} pricePerDay={pricePerDay} />
+      <Link href="/" className={`${styles.btn} button button--primary`}>
+        На головну
+      </Link>
+    </section>
   );
 };
 
-export default ToolDetails;
+export default BookingToolPage;
