f2af16850609466f230a0f9e684144c7e259356c
Iryna-M-html <ivmohnataya@gmail.com>
2025-12-21 18:43:36 +0200

    patch tools

--- app/(private routes)/manage-tools/[toolId]/edit/page.tsx
+++ app/(private routes)/manage-tools/[toolId]/edit/page.tsx
@@ -1,9 +1,30 @@
 import AddEditToolForm from '@/components/AddEditToolForm/AddEditToolForm';
+import { getToolById } from '@/lib/api/tools';
+type PageEditToolProps = {
+  params: Promise<{
+    toolId: string;
+  }>;
+};
 
-const EditToolsPage = () => {
+const EditToolsPage = async ({ params }: PageEditToolProps) => {
+  const { toolId } = await params;
+  const tool = await getToolById(toolId);
+  if (!tool) {
+    return <p>Інструмент не знайдено</p>;
+  }
+  const initialData = {
+    name: tool.name || '',
+    pricePerDay: tool.pricePerDay || '',
+    category: tool.category || '',
+    rentalTerms: tool.rentalTerms || '',
+    description: tool.description || '',
+    specifications: tool.specifications || '',
+    photo: tool.images || '',
+  };
+
   return (
     <section>
-      <AddEditToolForm />
+      <AddEditToolForm initialData={initialData} />
     </section>
   );
 };
