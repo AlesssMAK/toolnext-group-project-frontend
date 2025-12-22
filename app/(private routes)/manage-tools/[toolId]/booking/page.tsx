e0f08bbd0510336009847fa629ed067cd336d053
Oksana Skakun <ksanas180882@gmail.com>
2025-12-22 23:04:48 +0200

    metadata supplement

--- app/(private routes)/manage-tools/[toolId]/booking/page.tsx
+++ app/(private routes)/manage-tools/[toolId]/booking/page.tsx
@@ -1,22 +1,37 @@
+import type { Metadata } from 'next';
 import BookingToolForm from '@/components/BookingToolForm/BookingToolForm';
 import css from './BookingToolPage.module.css';
 import { getToolById } from '@/lib/api/tools';
 
 type PageProps = {
-  params: Promise<{
+  params: {
     toolId: string;
-  }>;
+  };
 };
 
+export async function generateMetadata(
+  { params }: PageProps
+): Promise<Metadata> {
+  const tool = await getToolById(params.toolId);
+
+  return {
+    title: `Бронювання: ${tool.name}`,
+    description: `Забронюйте інструмент "${tool.name}" за ${tool.pricePerDay} грн на день.`,
+  };
+}
+
 const BookingToolPage = async ({ params }: PageProps) => {
-  const { toolId } = await params;
-  const tool = await getToolById(toolId);
+  const tool = await getToolById(params.toolId);
 
   return (
     <section className={css.containerConfirmation}>
-      <BookingToolForm toolId={toolId} pricePerDay={tool.pricePerDay} />
+      <BookingToolForm
+        toolId={params.toolId}
+        pricePerDay={tool.pricePerDay}
+      />
     </section>
   );
 };
 
 export default BookingToolPage;
+
