7d63862b90686d82994be029101b109e71a616bb
Oleksii Makovii <makovii88@gmail.com>
2025-12-23 19:46:16 +0100

    Revert "Convert logout page to dynamic import"

--- app/(public routes)/confirm/logout/page.tsx
+++ app/(public routes)/confirm/logout/page.tsx
@@ -1,11 +1,1 @@
-import dynamic from 'next/dynamic';
-
-const LogoutModalPage = dynamic(
-  () => import('@/app/@modal/(.)confirm/logout/page'),
-  { ssr: false }
-);
-
-export default function Page() {
-  return <LogoutModalPage />;
-}
-
+export { default } from '@/app/@modal/(.)confirm/logout/page';
